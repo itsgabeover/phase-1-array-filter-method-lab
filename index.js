81b4041f2972f9799c43b00810f76b7ada929aed
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-08-13 18:58:55 -0700

    commit tree 6db0f0eabcab034f6082eca2406dcf62de7b39b7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(anArray, aString) {
+    return anArray.filter(name => name.toUpperCase() === aString.toUpperCase());
+}
+
+function fuzzyMatch(anArray, aString) {
+    return anArray.filter(name => name.startsWith(aString));
+}
+
+function matchName(objectArray, aString) {
+    return objectArray.filter(object => object.name === aString);
+}
